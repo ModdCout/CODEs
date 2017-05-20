1b3159e51a88586bec8a7418be1f7fca0ed40290
ModdCout <moddlabs@gmail.com>
2017-05-20 16:56:04 +0300

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+cout << "(unsigned) int = " << sizeof(int) << " byte" << endl;
+cout << "(unsigned) float = " << sizeof(float) << " byte" << endl;
+cout << "(unsigned) short int = " << sizeof(short int) << " byte" << endl;
+cout << "(unsigned) long int = " << sizeof(long int) << " byte" << endl;
+cout << "(unsigned) char = " << sizeof(char) << " byte" << endl;
+cout << "(unsigned) double = " << sizeof(double) << " byte" << endl;
+cout << "(unsigned) long double = " << sizeof(long double) << " byte" << endl;
+
+
+
+
+  return 0;
+}
